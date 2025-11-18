82f95a2399e1268bbceffd42a02fbd60625bec4b
Sylwester Zieliński <127629548+syziel1@users.noreply.github.com>
2025-11-18 23:08:27 +0100

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Bolt.new Template Ready ⚡ by syziel</h1>
+      <h1 className="text-4xl font-bold">Bolt.new App Ready ⚡ by syziel</h1>
     </div>
   );
 }
